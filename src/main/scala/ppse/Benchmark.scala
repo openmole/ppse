547709a1396ec9f6191be9887a9d199fcc49af36
Romain Reuillon <romain.reuillon@iscpif.fr>
2020-09-11 15:21:28 +0200

    Start implementing uniform sampling

--- src/main/scala/ppse/Benchmark.scala
+++ src/main/scala/ppse/Benchmark.scala
@@ -2,4 +2,9 @@ package ppse
 
 object Benchmark {
 
+  def exp(p: Vector[Double]): Vector[Double] = p.map(math.exp)
+
+
+  def pattern(f: Vector[Double] => Vector[Double]): Vector[Double] => Vector[Int] = ???
+
 }
---
+++ src/main/scala/ppse/Sampling.scala
@@ -0,0 +1,17 @@
+package ppse
+
+import scala.util.Random
+
+object Sampling {
+
+  type DensityMap = Vector[Vector[Double]]
+
+  def uniform(f: Vector[Double] => Vector[Double], points: Int, random: Random) = {
+
+
+
+
+  }
+
+
+}
