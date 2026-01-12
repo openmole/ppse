d30e823f38172bd4140986d00f6ead2dd7f5351e
Julien Perret <julien.perret@gmail.com>
2026-01-12 14:55:58 +0100

    mutate function

--- ppse-categorial/src/main/scala/ppse/categorial/ppse.scala
+++ ppse-categorial/src/main/scala/ppse/categorial/ppse.scala
@@ -1,7 +1,14 @@
 package ppse.categorial
 
-import scala.util.Random
+import org.apache.commons.math3.distribution.{EnumeratedDistribution, MixtureMultivariateNormalDistribution, MultivariateNormalDistribution}
+import org.apache.commons.math3.util.{MathArrays, Pair}
+import org.apache.commons.math3.linear.DiagonalMatrix
+import org.apache.commons.math3.linear.MatrixUtils
 
+import java.util
+import scala.util.Random
+import scala.jdk.CollectionConverters.*
+import ppse.paper.tool
 /*
  * Copyright (C) 2025 Romain Reuillon
  *
@@ -19,7 +26,7 @@ import scala.util.Random
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-type Genome = Vector[Double]
+type Genome = (Vector[Double], Double)
 type Pattern = Vector[Int]
 type HitMap = Map[Vector[Int], Int]
 case class Individual(genome: Genome, pattern: Pattern)
@@ -34,3 +41,29 @@ def breeding(population: Vector[Individual], hitMap: HitMap, size: Int, random: 
   ???
 
 
+/*
+def choose(population: Vector[Genome], weights: Vector[Double], random: Random): (Genome, Double) =
+  val randomValue = random.nextDouble()
+  val probabilities = MathArrays.normalizeArray(weights.toArray, 1.0)
+  val cumulativeProbabilities = probabilities.foldLeft(Array(0.0))((a,p)=> a.appended(a.last + p)).tail
+  val index = {
+    val r = util.Arrays.binarySearch(cumulativeProbabilities, randomValue)
+    if r < 0 then -r - 1 else r
+  }
+  assert(index >=0 && index < probabilities.length && randomValue < cumulativeProbabilities(index))
+  (population(index), probabilities(index))
+*/
+
+def mutate(population: Vector[Genome], genomeSize: Int, weights: Vector[Double], sigmaMax: Double, random: Random): Genome =
+  val apacheRandom = tool.toApacheRandom(random)
+  val comp = (population zip weights).map((p,w) => new Pair[Vector[Double], java.lang.Double](p._1,w)).toList.asJava
+  val dist = new EnumeratedDistribution[Vector[Double]](apacheRandom, comp)
+  // We generate a covariance matrix using an identity matrix multiplied by the sigma
+  val matrix = MatrixUtils.createRealIdentityMatrix(genomeSize).scalarMultiply(sigmaMax).getData
+  val list = population.zip(weights).map((g,w) => new Pair[java.lang.Double,MultivariateNormalDistribution](w, new MultivariateNormalDistribution(g._1.toArray,matrix))).toList.asJava
+  val mixture = new MixtureMultivariateNormalDistribution(apacheRandom, list)
+  val choice = mixture.sample()
+  (choice.toVector, mixture.density(choice))
+
+@main def mutateTest(): Unit =
+  mutate(Vector((Vector(0),0.1),(Vector(1),0.5),(Vector(2),0.4)),1,Vector(0.1,0.5,0.4),0.1,Random(42))
